f5d02ebb0c1b38bf56224c49edede356bfcdfcd8
Cauê Santos <34218434+cauebasantos@users.noreply.github.com>
2018-11-07 12:03:23 -0200

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,20 @@
+
+print('---- Summer Son\'s Mad Libs ----')
+print('You are going to give me a few inputs a la Mad Libs')
+print('Then I will prompt to you a full story')
+print('Have a good fun')
+print('--------------------------------')
+print()
+
+person = input('Give me a person:    ')
+place = input('And now a place:    ')
+adjective = input('A adjective:    ')
+plural_noun = input('Now a plural noun:    ')
+
+print()
+
+story = f'Yesterday I saw {person} at the {place} dancing with some {plural_noun}. They were {adjective}!'
+print(story)
+
+print()
+print('Thank you for playing the game')
